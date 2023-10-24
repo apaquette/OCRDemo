a28eec984838ad0fbd7e2a67e669613922e52229
Alex <alexandre.d.paquette@gmail.com>
2023-10-24 14:57:39 +0100

    Testing out different OcrInput options

--- Program.cs
+++ Program.cs
@@ -8,13 +8,15 @@ void ConvertToText(string imagePath)
         input.DeNoise();
         input.Deskew();
         input.EnhanceResolution();
+        //input.Contrast();
+        input.Invert();
         var Result = IronOcr.Read(input);
         Console.WriteLine(Result.Text);
         Console.WriteLine("---------------------");
     }
 }
 
-string[] files = Directory.GetFiles("D:\\SETU\\Project C4\\Remote Energy Meter\\OCR Demo\\OCRDemo\\Photos");
+string[] files = Directory.GetFiles("D:\\SETU\\Project C4\\Remote Energy Meter\\OCR Demo\\OCRDemo\\Photos_Cropped");
 
 
 foreach(var file in files)
