5220aed1a932e307c0aaf74eb8ecee972264ae88
Alex <alexandre.d.paquette@gmail.com>
2023-10-24 14:18:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using IronOcr;
+
+void ConvertToText(string imagePath)
+{
+    IronTesseract IronOcr = new();
+    using(OcrInput input = new(imagePath))
+    {
+        input.DeNoise();
+        input.Deskew();
+        input.EnhanceResolution();
+        var Result = IronOcr.Read(input);
+        Console.WriteLine(Result.Text);
+        Console.WriteLine("---------------------");
+    }
+}
+
+string[] files = Directory.GetFiles("D:\\SETU\\Project C4\\Remote Energy Meter\\OCR Demo\\OCRDemo\\Photos");
+
+
+foreach(var file in files)
+{
+    ConvertToText(file);
+}
